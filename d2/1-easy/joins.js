093026cb0fa269feacc9b108912de53e40c74f74
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-11-02 14:10:42 +0000

    Update joins.js

--- d2/1-easy/joins.js
+++ d2/1-easy/joins.js
@@ -12,22 +12,37 @@
 const { db } = require('../db/db');
 
 async function employeeNameAndJobTitle() {
-  const test = await db.query(``);
-  console.log(test);
+  const [test] = await db.query(`
+    SELECT employee_details.name, current_job_details.job_title
+    FROM employee_details INNER JOIN current_job_details
+    ON employee_details.employee_id = current_job_details.employee_id
+  `);
+  console.table(test);
 }
 
-employeeNameAndJobTitle();
+// employeeNameAndJobTitle();
 
 async function employeeOS() {
-  const test = await db.query(``);
-  console.log(test);
+  const [test] = await db.query(`
+    SELECT employee_details.name, laptop_details.os
+    FROM employee_details LEFT JOIN current_job_details
+    ON employee_details.employee_id = current_job_details.employee_id
+    INNER JOIN laptop_details
+    ON current_job_details.laptop_id = laptop_details.laptop_id
+  `);
+  console.table(test);
 }
 
-employeeOS();
+// employeeOS();
 
 async function employeeNameAndPreviousJobTitles() {
-  const test = await db.query(``);
-  console.log(test);
+  const [test] = await db.query(`
+    SELECT employee_details.name, histories.job_title
+    FROM employee_details INNER JOIN histories
+    ON employee_details.employee_id = histories.employee_id
+    ORDER BY employee_details.name
+  `);
+  console.table(test);
 }
 
 employeeNameAndPreviousJobTitles();
